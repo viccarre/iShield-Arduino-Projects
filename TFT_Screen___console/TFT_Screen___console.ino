6c0c2010ba36eee23c03032ffa66a9ce7650296d
viccarre <viccarre@gmail.com>
2015-07-27 22:44:12 -0400

    sending data to the iOS device when a button is pressed/released

--- TFT_Screen___console/TFT_Screen___console.ino
+++ TFT_Screen___console/TFT_Screen___console.ino
@@ -7,8 +7,8 @@
 
 
 #if defined(__SAM3X8E__)
-    #undef __FlashStringHelper::F(string_literal)
-    #define F(string_literal) string_literal
+#undef __FlashStringHelper::F(string_literal)
+#define F(string_literal) string_literal
 #endif
 
 // These are the pins used for the UNO
@@ -24,22 +24,28 @@
 Adafruit_ILI9340 tft = Adafruit_ILI9340(_cs, _dc, _mosi, _sclk, _rst, _miso);
 // Use hardware SPI
 //Adafruit_ILI9340 tft = Adafruit_ILI9340(_cs, _dc, _rst);
+
 iShield myiShield;
+int buttonPin = 1;
+bool flag = false;
+bool flag2 = false;
+
 String message;
 
 void setup() {
-  
-  ble_begin();
+
+  pinMode(buttonPin, INPUT);
+
   Serial.begin(9600);
-  Serial.println("Setup BLE ...");
-  ble_begin();
+
   Serial.println("Setup TFT Screen ...");
   tft.begin();
-  Serial.println("Finish setup ...");
-  //ble_begin();
+  Serial.println("Setup BLE ...");
+  ble_begin();
   tft.fillScreen(ILI9340_BLACK);
   tft.setRotation(3);
   tft.setCursor(0, 0);
+  Serial.println("Finish setup ...");
 
 }
 
@@ -48,18 +54,33 @@ void loop(void) {
 
   myiShield.readConsole();
   message = myiShield.incomingData();
-  tft.fillRect(0,22,ILI9340_TFTHEIGHT,40,ILI9340_BLACK);
+
+  if(digitalRead(A1) == HIGH && flag == false){
+    myiShield.writeConsole("RED Pressed");
+    flag = true;
+  }
+  else if(digitalRead(A1) == LOW && flag == true){
+    myiShield.writeConsole("RED Released");
+    flag = false;
+  }
+
+  if(digitalRead(A0) == HIGH && flag2 == false){
+    myiShield.writeConsole("GREEN Pressed");
+    flag2 = true;
+  }
+  else if(digitalRead(A0) == LOW && flag2 == true){
+    myiShield.writeConsole("GREEN Released");
+    flag2 = false;
+  }
+
+  tft.fillRect(0,22,ILI9340_TFTHEIGHT,45,ILI9340_BLACK);
   tft.setTextColor(ILI9340_GREEN);
   tft.setTextSize(3);
-  tft.setCursor(0, 0);
-  tft.println("iShield data > ");
-  tft.println("                     ");
   tft.setCursor(0, 0);
   tft.setTextColor(ILI9340_GREEN);
   tft.setTextSize(3);
   tft.println("iShield data > ");
   tft.println(message);
-  Serial.println(message);
-  
 }
+
 
